5ee8472b0c49296bb1f94f59b8133ed8936fc60e
hshqwq <77627831+hshqwq@users.noreply.github.com>
2021-04-01 00:37:33 +0800

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", () => {
+    let windowW = window.innerWidth;
+    let windowH = window.innerHeight;
+    document.getElementById("headImg").style.padding = "500";
+})
