af475b12dbf75e326f7fdb34cfccb613ce88528a
hshqwq <77627831+hshqwq@users.noreply.github.com>
2021-04-01 22:17:51 +0800

    Delete index.js

--- JS/index.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load", () => {
-    let windowW = window.innerWidth;
-    let windowH = window.innerHeight;
-    document.getElementById("headImg").style.padding = "500";
-})
