be83cb2e8bdb9706df63ad53c2406d1790ca5d60
hshqwq <77627831+hshqwq@users.noreply.github.com>
2021-09-06 22:15:27 +0800

    Add files via upload

--- tools/clean bomb/main.js
+++ tools/clean bomb/main.js
@@ -1,12 +1,12 @@
 function gamestart(mapWidth, mapHeight, theNuberOfBombs) {
 	// function
 	function countCDN(x, y) {
-		return x + y * Math.max(width, height);
+		return x + y * width;
 	}
 	function gameOver() {
 		let notOpen = 0;
 		for(let i = 0;i < mask.length;i++) {
-			if(mask[i] == 0) {
+			if(mask[i] == 0 || mask[i] == 2) {
 				notOpen += 1;
 			}
 			if((map[i] == 9) && mask[i] == 1){
@@ -19,27 +19,47 @@ function gamestart(mapWidth, mapHeight, theNuberOfBombs) {
 			}
 		}
 		if(notOpen == bomb) {
-			document.body.innerHTML = '<h1>You Win</h1>';
+			marks += bomb;
+			document.querySelector('#marks').innerHTML = marks;
+			for(let i = 0;i < width * height;i++) {
+				if(mask[i] != 2) {
+					mask[i] = 1;
+				}
+			}
+			print();
+			document.body.innerHTML += '<h1>You Win</h1>';
 			return 'win';
 		}
 		return null;
 	}
 	function print(){
-		document.body.innerHTML = '';
 		let prints;
-		for(let i = 0;i < width;i++) {
-			for(let j = 0;j < height;j++) {
+		for(let i = 0;i < height;i++) {
+			for(let j = 0;j < width;j++) {
 				if(mask[countCDN(j, i)] == 0) {
-					prints = '■';
+					document.querySelector(`#block_${countCDN(j, i)}`).style.backgroundColor = '#eee';
+					document.querySelector(`#block_${countCDN(j, i)}`).style.color = '#fff0';
+				} else if(mask[countCDN(j, i)] == 2) {
+					document.querySelector(`#block_${countCDN(j, i)}`).style.backgroundColor = '#eee';
+					document.querySelector(`#block_${countCDN(j, i)}`).style.color = 'black';
+					document.querySelector(`#block_${countCDN(j, i)}`).innerHTML = '🚩';
 				} else if(map[countCDN(j, i)] == 9) {
-					prints = '○';
+					document.querySelector(`#block_${countCDN(j, i)}`).innerHTML = '💣';
+					document.querySelector(`#block_${countCDN(j, i)}`).style.backgroundColor = 'white';
+					document.querySelector(`#block_${countCDN(j, i)}`).style.color = 'black';
 				} else {
-					prints = map[countCDN(j, i)];
+					document.querySelector(`#block_${countCDN(j, i)}`).innerHTML = map[countCDN(j, i)];
+					document.querySelector(`#block_${countCDN(j, i)}`).style.backgroundColor = 'white';
+					document.querySelector(`#block_${countCDN(j, i)}`).style.color = 'black';
 				}
-				
-				document.body.innerHTML += `${countCDN(mouse.x, mouse.y) == countCDN(j, i) ? '&nbsp;>' : '&nbsp;&nbsp;&nbsp;'}${prints}`;
+				for(let i = 0;i < width * height;i++) {
+					if(i == countCDN(mouse.x, mouse.y)) {
+						document.querySelector(`#block_${i}`).style.outline = '1px solid red';
+					} else {
+						document.querySelector(`#block_${i}`).style.outline = 'none';
+					}
+				}
 			}
-			document.body.innerHTML += `<br>`;
 		}
 	}
 	function setMouseDNS(x, y){
@@ -94,55 +114,99 @@ function gamestart(mapWidth, mapHeight, theNuberOfBombs) {
 				}
 			}
 		}
+		marks = 0;
+		for(let i = 0;i < width * height; i++) {
+			if(map[i] != 9 && mask[i] == 1) {
+				marks += 1;
+			}
+		}
+		document.querySelector('#marks').innerHTML = marks;
+		print();
+		gameOver();
 	}
 	
 	// init
-	let mouse = {'x': 0, 'y': 0}, width = mapWidth, height = mapHeight, bomb = theNuberOfBombs, map = new Array, mask = new Array;
+	let mouse = {'x': 0, 'y': 0}, width = Number(mapWidth), height = Number(mapHeight), bomb = Number(theNuberOfBombs), map = new Array, mask = new Array, marks = 0, flags = 0;
+	
+	document.body.innerHTML = `<h1 id="title">扫雷</h1><div id="blockBox" style="width: ${width * 18}px;"><div id="messageBox"><div id="messageName"><span>分数</span><span>剩余雷数</span></div><div><span id="marks">0</span><span id="nuberOfBombs">0</span></div></div><div id="blocks" style="width: ${width * 18}px;height: ${height * 18}px;"></div></div>`;
 	for(let i = 0;i < width * height;i++) {
 		map[i] = 0;
 		mask[i] = 0;
+		document.querySelector('#blocks').innerHTML += `<span id="block_${i}" class="block"></span>`
 	}
+	document.querySelector('#nuberOfBombs').innerHTML = bomb;
+	document.querySelector('#blocks').oncontextmenu = (e) => {return false;}
 	// set bomb
 	for(let i = 0;i < bomb;i++) {
-		let l = new Array(Math.round(Math.random() * width), Math.round(Math.random() * height));
+		let l = new Array(Math.round(Math.random() * (width - 1)), Math.round(Math.random() * (height - 1)));
 		let j = countCDN(l[0], l[1]);
-		console.log(l[0] + l[1]);
 		if(map[j] == 9){
 			i -= 1;
 		} else {
 			map[j] = 9;
-			if(map[countCDN(l[0] - 1, l[1] - 1)] != 9) {map[countCDN(l[0] - 1, l[1] - 1)] += 1;}
-			if(map[countCDN(l[0], l[1] - 1)] != 9) {map[countCDN(l[0], l[1] - 1)] += 1;}
-			if(map[countCDN(l[0] + 1, l[1] - 1)] != 9) {map[countCDN(l[0] + 1, l[1] - 1)] += 1;}
-			if(map[countCDN(l[0] - 1, l[1])] != 9) {map[countCDN(l[0] - 1, l[1])] += 1;}
-			if(map[countCDN(l[0] + 1, l[1])] != 9) {map[countCDN(l[0] + 1, l[1])] += 1;}
-			if(map[countCDN(l[0] - 1, l[1] + 1)] != 9) {map[countCDN(l[0] - 1, l[1] + 1)] += 1;}
-			if(map[countCDN(l[0], l[1] + 1)] != 9) {map[countCDN(l[0], l[1] + 1)] += 1;}
-			if(map[countCDN(l[0] + 1, l[1] + 1)] != 9) {map[countCDN(l[0] + 1, l[1] + 1)] += 1;}
+			if(l[0] != 0) {
+				if(map[countCDN(l[0] - 1, l[1])] != 9) {map[countCDN(l[0] - 1, l[1])] += 1;}
+			}
+			if(l[0] != width - 1) {
+				if(map[countCDN(l[0] + 1, l[1])] != 9) {map[countCDN(l[0] + 1, l[1])] += 1;}
+			}
+			if(l[1] != 0) {
+				if(map[countCDN(l[0], l[1] - 1)] != 9) {map[countCDN(l[0], l[1] - 1)] += 1;}
+			}
+			if(l[1] != height - 1) {
+				if(map[countCDN(l[0], l[1] + 1)] != 9) {map[countCDN(l[0], l[1] + 1)] += 1;}
+			}
+			if(l[0] != 0 && l[1] != 0) {
+				if(map[countCDN(l[0] - 1, l[1] - 1)] != 9) {map[countCDN(l[0] - 1, l[1] - 1)] += 1;}
+			}
+			if(l[0] != width - 1 && l[1] != height - 1) {
+				if(map[countCDN(l[0] + 1, l[1] + 1)] != 9) {map[countCDN(l[0] + 1, l[1] + 1)] += 1;}
+			}
+			if(l[0] != width - 1 && l[1] != 0) {
+				if(map[countCDN(l[0] + 1, l[1] - 1)] != 9) {map[countCDN(l[0] + 1, l[1] - 1)] += 1;}
+			}
+			if(l[0] != 0 && l[1] != height - 1) {
+				if(map[countCDN(l[0] - 1, l[1] + 1)] != 9) {map[countCDN(l[0] - 1, l[1] + 1)] += 1;}
+			}	
 		}
 	}
 	print();
+	for(let i = 0;i < width * height;i++) {
+		document.querySelector(`#block_${i}`).onmouseover = () => {
+			mouse.x = i % width;
+			mouse.y = (i - (i % width)) / width;
+			print();
+		}
+	}
+	document.querySelector('#blocks').onmousedown = (event) => {
+		if(event.button == 0) {
+			openDNS(mouse.x, mouse.y);
+		} else if(event.button == 2) {
+			mask[countCDN(mouse.x, mouse.y)] == 0 ? mask[countCDN(mouse.x, mouse.y)] = 2 : mask[countCDN(mouse.x, mouse.y)] = 0;
+			print();
+			flags = 0;
+			for(let i = 0;i < width * height;i++) {
+				if(mask[i] == 2) {
+					flags += 1;
+				}
+			}
+			document.querySelector('#nuberOfBombs').innerHTML = bomb - flags;
+		}
+	}
 	window.onkeyup = (event) => {
-		console.clear();
-		console.log(event.keyCode);
 		if(mask[0] != 3) {
 			if(event.keyCode == 87) {
 				addMouseDNS(0, -1);
-				console.log(mouse.y);
 			} else if(event.keyCode == 65) {
 				addMouseDNS(-1, 0);
-				console.log(mouse.x);
 			} else if(event.keyCode == 83) {
 				addMouseDNS(0, 1);
-				console.log(mouse.x);
 			} else if(event.keyCode == 68) {
 				addMouseDNS(1, 0);
-				console.log(mouse.x);
 			} else if(event.keyCode == 32) {
 				openDNS(mouse.x, mouse.y);
 			}
 			print();
-			gameOver();
 		}
 		
 	}
